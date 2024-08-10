7bb6c5eb7ec614865d383383a6b64efee3838ae1
Shilpa <64268141+Shilpa40@users.noreply.github.com>
2024-08-10 18:17:21 +0530

    Removing Extra TestCases

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -27,10 +27,6 @@ public class TestGreeter {
     assertThat(greeter.greet(someone), containsString(someone));
   }
   
-@Test
-  public void intentially_Failed() {
-    assertTrue(true);
-  }
   @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
